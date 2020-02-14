d4ee79b22493c17409dc73e9e5181c5048eb39f5
user1 <user1@LP-5CD9296CX3.HCLT.CORP.HCL.IN>
2020-02-14 16:26:13 +0530

    sprint 1

--- src/app/donor/components/donor-home/donor-home.component.ts
+++ src/app/donor/components/donor-home/donor-home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpService } from '../../../shared/services/http.service';
 import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-donor-home',
@@ -13,14 +14,16 @@ export class DonorHomeComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private http: HttpService, private route: Router) { }
 
   public donorForm = new FormGroup({
-    schemename: new FormControl(),
-    amount: new FormControl(),
-    description: new FormControl(),
-    donorname: new FormControl(),
-    donoremail: new FormControl(),
-    donorpan: new FormControl(),
-    donorcrdr: new FormControl(),
+    donationSchemeId: new FormControl(),
+    donorName: new FormControl(),
+    donorEmail: new FormControl(),
+    panCard: new FormControl(),
+    cardNo: new FormControl(),
   });
+  public listOfSchemes: Array<any>;
+  public donationAmount: number;
+  public taxBenefit: number;
+  public description: string = '';
   ngOnInit() {
     this.createForm();
     this.getSchemeNames();
@@ -28,7 +31,7 @@ export class DonorHomeComponent implements OnInit {
 
     getSchemeNames() {
       this.http.getRequest('/donations/schemes').subscribe((data) => {
-        console.log('data', data);
+        this.listOfSchemes = data.listOfSchemes;
       });
     }
 
@@ -38,31 +41,43 @@ export class DonorHomeComponent implements OnInit {
     // create a reactive login form
     createForm() {
       this.donorForm = this.formBuilder.group({
-        schemename: ['', [
+        donationSchemeId: ['', [
           Validators.required]],
-        amount: ['', [
+        donorName: ['', [
           Validators.required,
           Validators.minLength(2)]],
-        description: ['', [
+        donorEmail: ['', [
           Validators.required,
-          Validators.minLength(2)]],
-        donorname: ['', [
+          Validators.minLength(2),
+          Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)]],
+        panCard: ['', [
           Validators.required,
-          Validators.minLength(2)]],
-        donoremail: ['', [
-          Validators.required,
-          Validators.minLength(2)]],
-        donorpan: ['', [
-          Validators.required,
-          Validators.minLength(2)]],
-        donorcrdr: ['', [
+          Validators.minLength(2),
+          Validators.pattern(/^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$/)]],
+        cardNo: ['', [
           Validators.required,
           Validators.minLength(2)]]
       });
     }
 
     doPayment() {
-      console.log(this.donorForm.value);
+      const payload = this.donorForm.value;
+      payload.cardType = 'Debit card';
+      payload.taxBenefitAmount = this.taxBenefit.toString();
+      this.http.postRequest('/donations/paymentDetails', payload).subscribe((data) => {
+        swal.fire('Success');
+      }, (exception) => {
+        swal.fire('Fail');
+      });
+    }
+
+    updateDonationAmount() {
+      const filteredObject = this.listOfSchemes.find((data) => {
+        return Number(this.donorForm.value.donationSchemeId) === Number(data.donationSchemeId);
+      });
+      this.donationAmount = filteredObject.donationAmount;
+      this.taxBenefit = filteredObject.donationAmount * filteredObject.taxBenefitPer;
+      this.description = filteredObject.description;
     }
 
 }
